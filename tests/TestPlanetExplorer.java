73c92b52ac5678e9a584ac964b2e02b23abc0433
somename <someemail>
2016-10-21 15:05:44 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -10,4 +10,11 @@ public class TestPlanetExplorer {
 		String start = test.executeCommand(null);
 		assertEquals("0,0,N", start);
 	}
+	
+	@Test
+	public void test_command_foward() {
+		PlanetExplorer test = new PlanetExplorer(100, 100, null);
+		String start = test.executeCommand("f");
+		assertEquals("0,0,N", start);
+	}
 }
