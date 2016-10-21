62cab1599014e1ba348144438e13b322a1fd7714
somename <someemail>
2016-10-21 15:38:38 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -43,6 +43,7 @@ public class PlanetExplorer {
 						case 'l' : facing = 'W';
 						case 'r' : facing = 'E';
 						};
+						/*
 			case 'E' : 	switch (order) {
 						case 'f' : pos_x = pos_x + 1;
 						case 'b' : pos_x = pos_x - 1;
@@ -61,6 +62,7 @@ public class PlanetExplorer {
 						case 'l' : facing = 'S';
 						case 'r' : facing = 'N';
 						};
+						*/
 			}
 		}
 		String journey = "(" + pos_x + "," + pos_y + "," + facing + ")";
