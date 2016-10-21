febc546bb19d91538b4a2a848a641e0c81b2ed3c
somename <someemail>
2016-10-21 15:42:44 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -42,26 +42,27 @@ public class PlanetExplorer {
 						case 'b' : pos_y = pos_y - 1;
 						case 'l' : facing = 'W';
 						case 'r' : facing = 'E';
-						}
+						};
 			case 'E' : 	switch (order) {
 						case 'f' : pos_x = pos_x + 1;
 						case 'b' : pos_x = pos_x - 1;
 						case 'l' : facing = 'N';
 						case 'r' : facing = 'S';
-						}
+						};
 			case 'S' : 	switch (order) {
 						case 'f' : pos_y = pos_y - 1;
 						case 'b' : pos_y = pos_y + 1;
 						case 'l' : facing = 'E';
 						case 'r' : facing = 'W';
-						}
+						};
 			case 'W' : 	switch (order) {
 						case 'f' : pos_x = pos_x - 1;
 						case 'b' : pos_x = pos_x + 1;
 						case 'l' : facing = 'S';
 						case 'r' : facing = 'N';
-						}
+						};
 			}
+			System.out.println(pos_x + "," + pos_y + "," + facing);
 		}
 		String journey = "(" + pos_x + "," + pos_y + "," + facing + ")";
 		return journey;
